a46840e93d6f0e0908289e84f012792821b1502d
小歪 <huiquhome@163.com>
2014-09-26 22:11:19 +0800

    Create Date.swift
    let now = Date()

---
+++ Date.swift
@@ -0,0 +1,179 @@
+//
+//  Date.swift
+//
+//  Created by 慧趣小歪 on 14/9/26.
+//
+//  值类型的 Date 使用方便 而且避免使用 @NSCopying 的麻烦 
+//  基本遵循了官方所有关于值类型的实用协议 放心使用
+//
+
+import Foundation
+
+func ==(lhs: Date, rhs: Date) -> Bool {
+    return lhs.timeInterval == rhs.timeInterval
+}
+func <=(lhs: Date, rhs: Date) -> Bool {
+    return lhs.timeInterval <= rhs.timeInterval
+}
+func >=(lhs: Date, rhs: Date) -> Bool {
+    return lhs.timeInterval >= rhs.timeInterval
+}
+func >(lhs: Date, rhs: Date) -> Bool {
+    return lhs.timeInterval > rhs.timeInterval
+}
+func <(lhs: Date, rhs: Date) -> Bool {
+    return lhs.timeInterval < rhs.timeInterval
+}
+
+func +=(inout lhs: Date, rhs: NSTimeInterval) {
+    return lhs = Date(rhs, sinceDate:lhs)
+}
+func -=(inout lhs: Date, rhs: NSTimeInterval) {
+    return lhs = Date(-rhs, sinceDate:lhs)
+}
+
+
+struct Date {
+    var timeInterval:NSTimeInterval = 0
+    
+    init() { self.timeInterval = NSDate().timeIntervalSince1970 }
+}
+
+// MARK: - 输出
+extension Date {
+    func stringWithFormat(format:String = "yyyy-MM-dd HH:mm:ss") -> String {
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = format
+        return formatter.stringFromDate(NSDate(timeIntervalSince1970: timeInterval))
+    }
+}
+
+extension Date {
+    
+    // for example : let (year, month, day) = date.getDay()
+    func getDay() -> (Int, Int, Int) {
+        var year:Int = 0, month:Int = 0, day:Int = 0
+        let date = NSDate(timeIntervalSince1970: timeInterval)
+        NSCalendar.currentCalendar().getEra(nil, year: &year, month: &month, day: &day, fromDate: date)
+        return (year, month, day)
+    }
+    
+    // for example : let (hour, minute, second) = date.getTime()
+    func getTime() -> (Int, Int, Int) {
+        var hour:Int = 0, minute:Int = 0, second:Int = 0
+        let date = NSDate(timeIntervalSince1970: timeInterval)
+        NSCalendar.currentCalendar().getHour(&hour, minute: &minute, second: &second, nanosecond: nil, fromDate: date)
+        return (hour, minute, second)
+    }
+}
+
+// MARK: - 构造函数
+extension Date {
+    init(year:Int, month:Int = 0, day:Int = 1, hour:Int = 1, minute:Int = 0, second:Int = 0) {
+        let era = year / 100
+        if let date = NSCalendar.currentCalendar().dateWithEra(era, year: year, month: month, day: day, hour: hour, minute: minute, second: second, nanosecond: 0) {
+            timeInterval = date.timeIntervalSince1970
+        }
+    }
+}
+
+extension Date {
+    init(_ v: NSTimeInterval) { timeInterval = v }
+
+    init(_ v: NSTimeInterval, sinceDate:Date) {
+        let date = NSDate(timeIntervalSince1970: sinceDate.timeInterval)
+        timeInterval = NSDate(timeInterval: v, sinceDate: date).timeIntervalSince1970
+    }
+    
+    init(sinceNow: NSTimeInterval) {
+        timeInterval = NSDate(timeIntervalSinceNow: sinceNow).timeIntervalSince1970
+    }
+    
+    init(sinceReferenceDate: NSTimeInterval) {
+        timeInterval = NSDate(timeIntervalSinceReferenceDate: sinceReferenceDate).timeIntervalSince1970
+    }
+}
+
+extension Date {
+    init(_ v: String, style: NSDateFormatterStyle = .NoStyle) {
+        var formatter = NSDateFormatter()
+        formatter.dateStyle = style
+        if let date = formatter.dateFromString(v) {
+            self.timeInterval = date.timeIntervalSince1970
+        }
+    }
+    
+    init(_ v: String, dateFormat:String = "yyyy-MM-dd HH:mm:ss") {
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = dateFormat
+        if let date = formatter.dateFromString(v) {
+            self.timeInterval = date.timeIntervalSince1970
+        }
+    }
+}
+
+extension Date {
+    init(_ v: UInt8) { timeInterval = Double(v) }
+    init(_ v: Int8) { timeInterval = Double(v) }
+    init(_ v: UInt16) { timeInterval = Double(v) }
+    init(_ v: Int16) { timeInterval = Double(v) }
+    init(_ v: UInt32) { timeInterval = Double(v) }
+    init(_ v: Int32) { timeInterval = Double(v) }
+    init(_ v: UInt64) { timeInterval = Double(v) }
+    init(_ v: Int64) { timeInterval = Double(v) }
+    init(_ v: UInt) { timeInterval = Double(v) }
+    init(_ v: Int) { timeInterval = Double(v) }
+}
+
+extension Date {
+    init(_ v: Float) { timeInterval = Double(v) }
+    //init(_ v: Float80) { timeInterval = Double(v) }
+}
+
+// MARK: - 可以直接输出
+extension Date : Printable {
+    var description: String {
+        return NSDate(timeIntervalSince1970: timeInterval).description
+    }
+}
+extension Date : DebugPrintable {
+    var debugDescription: String {
+        return NSDate(timeIntervalSince1970: timeInterval).debugDescription
+    }
+}
+
+// MARK: - 可以直接赋值整数
+extension Date : IntegerLiteralConvertible {
+    static func convertFromIntegerLiteral(value: Int64) -> Date {
+        return Date(Double(value))
+    }
+}
+
+// MARK: - 可以直接赋值浮点数
+extension Date : FloatLiteralConvertible {
+    static func convertFromFloatLiteral(value: Double) -> Date {
+        return Date(value)
+    }
+}
+
+// MARK: - 可反射
+extension Date : Reflectable {
+    func getMirror() -> MirrorType {
+        return reflect(self)
+    }
+}
+
+// MARK: - 可哈希
+extension Date : Hashable {
+    var hashValue: Int { return timeInterval.hashValue }
+}
+
+// 可以用 == 或 != 对比
+extension Date : Equatable {
+    
+}
+
+// MARK: - 可以用 > < >= <= 对比
+extension Date : Comparable {
+
+}
